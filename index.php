d30673bab739d72fc4299be38741e820d70f212c
Tobias Heide <slepzs@gmail.com>
2019-04-08 10:22:34 +0200

    Added Navigation, new colors, and cards

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+include('includes/header.php');
+include('includes/navigation.php'); ?>
+
+
+    <div class="container">
+        <div class="row">
+
+
+
+      <div class="card text-center">
+        <div class="card-header">
+          Featured Cats
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Take a look at our special cats! </h5>
+          <p class="card-text">Cats world have a large amounts of cats for adoptions</p>
+          <a href="#" class="mybtn btn btn-primary">CATalog ;)</a>
+        </div>
+        <div class="card-footer text-muted">
+          2 days ago
+        </div>
+      </div>
+      
+        </div>
+    </div>
+
+
+
+<?php include('includes/footer.php'); ?>
